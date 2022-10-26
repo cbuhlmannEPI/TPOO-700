096e6374e5227f8d213b1545f6f33da4c1d759a2
cbuhlmannEPI <cyril.buhlmann@epitech.eu>
2022-10-26 11:40:50 +0200

    [+] api

---
+++ api/lib/api/users/user.ex
@@ -0,0 +1,21 @@
+defmodule Api.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :username, :string
+    has_many :clocks, Api.Clocks.Clock
+    has_many :workingtimes, Api.Workingtimes.Workingtime
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :email])
+    |> validate_required([:username, :email])
+    |> unique_constraint(:email)
+  end
+end
---
+++ api/lib/api_web/controllers/clock_controller.ex
@@ -0,0 +1,22 @@
+defmodule ApiWeb.ClockController do
+  use ApiWeb, :controller
+
+  alias Api.Clocks
+  alias Api.Clocks.Clock
+
+  action_fallback ApiWeb.FallbackController
+
+  def index(conn, %{"userID" => userID}) do
+    clocks = Clocks.list_clocks!(userID)
+    render(conn, "index.json", clocks: clocks)
+  end
+
+  def create(conn, %{"clock" => clock_params}) do
+    with {:ok, %Clock{} = clock} <- Clocks.create_clock(clock_params) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", Routes.clock_path(conn, :show, clock))
+      |> render("show.json", clock: clock)
+    end
+  end
+end
