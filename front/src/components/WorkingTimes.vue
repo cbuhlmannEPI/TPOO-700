a59c2b95861c2e24e9786be73a39d16a216d6799
Benjamin Bulone <benja.bulone@gmail.com>
2022-10-28 16:11:44 +0200

    [+] add working times table

--- front/src/components/WorkingTimes.vue
+++ front/src/components/WorkingTimes.vue
@@ -1,16 +1,33 @@
 <script lang="ts" setup></script>
 
 <template>
-    <h3>Installed CLI Plugins</h3>
-    <div>{{$route.params}}</div>
+  <h3>Installed CLI Plugins</h3>
+  <div>{{ $route.params }}</div>
+
+  <div class="WorkingTimesTable">
+    <table>
+      <tr>
+        <th>Test</th>
+        <th>Test</th>
+        <th>Test</th>
+      </tr>
+      <tr>
+        <td> test</td>
+        <td>test</td>
+        <td>test</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
-  
+
   name: 'WorkingTimes'
 }
 </script>
-<style scoped></style>
+<style scoped>
+
+</style>
 
  
